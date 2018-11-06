0e9c1de939cca17c7297a384414ff076f22770c7
zensky123 <zensky@126.com>
2018-11-06 21:33:18 +0800

    Add socket demo

---
+++ web/socket/UDPSimplest1/client.py
@@ -0,0 +1,19 @@
+from socket import *
+
+HOST = ''
+PORT = 21567
+BUFSIZE = 1024
+ADDR = (HOST, PORT)
+
+udpCliSock = socket(AF_INET, SOCK_DGRAM)
+
+while True:
+    data = input('> ')
+    if not data:
+        break
+    udpCliSock.sendto(bytes(data, 'utf-8'), ADDR)
+    data, ADDR = udpCliSock.recvfrom(BUFSIZE)
+    if not data:
+        break
+    print(data)
+udpCliSock.close()
