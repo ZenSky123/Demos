f1b61ee32dacfce5b42a9635c3e07e05ced8f14f
zensky123 <zensky@126.com>
2018-11-08 17:35:56 +0800

    Update django-rest-framework demo

--- web/django/tutorial/snippets/views.py
+++ web/django/tutorial/snippets/views.py
@@ -1,4 +1,8 @@
-from rest_framework import generics
+from django.urls import reverse
+from rest_framework import generics, renderers
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+
 from snippets.models import Snippet
 from snippets.serializers import SnippetSerializer, UserSerializer
 from snippets.permission import IsOwnerOrReadOnly
@@ -8,6 +12,23 @@ from rest_framework import permissions
 """
     Edition 5: 使用通用类视图
 """
+
+
+@api_view(['GET'])
+def api_root(request, format=None):
+    return Response({
+        'users': reverse('user-list', request=request, format=format),
+        'snippets': reverse('snippet-list', request=request, format=format)
+    })
+
+
+class SnippetHighlight(generics.GenericAPIView):
+    queryset = Snippet.objects.all()
+    renderer_classes = (renderers.StaticHTMLRenderer,)
+
+    def get(self, request, *args, **kwargs):
+        snippet = self.get_object()
+        return Response(snippet.highlighted)
 
 
 class UserList(generics.ListAPIView):
--- web/django/tutorial/tutorial/urls.py
+++ web/django/tutorial/tutorial/urls.py
@@ -15,7 +15,4 @@ from snippets import views
 urlpatterns = [
     # url(r'^', include(router.urls)),
     url(r'^', include('snippets.urls')),
-    url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
-    url(r'^user/$', views.UserList.as_view()),
-    url(r'^users/(?P<pk>[0-9]+)/%', views.UserDetail.as_view())
 ]
