b82db7fcb89f2383c13923434294cdb1d7f86ed6
zensky123 <zensky@126.com>
2018-11-08 16:25:04 +0800

    Update django-rest-framework demo

--- web/django/tutorial/snippets/views.py
+++ web/django/tutorial/snippets/views.py
@@ -1,23 +1,38 @@
-from django.http import Http404
-from rest_framework import status, mixins, generics
-from rest_framework.response import Response
-from rest_framework.views import APIView
+from rest_framework import generics
 from snippets.models import Snippet
-from snippets.serializers import SnippetSerializer
+from snippets.serializers import SnippetSerializer, UserSerializer
+from snippets.permission import IsOwnerOrReadOnly
+from django.contrib.auth.models import User
+from rest_framework import permissions
 
 """
     Edition 5: 使用通用类视图
 """
 
 
+class UserList(generics.ListAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+
+class UserDetail(generics.RetrieveAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+
 class SnippetList(generics.ListCreateAPIView):
     queryset = Snippet.objects.all()
     serializer_class = SnippetSerializer
+    permission_classes = (permissions.IsAuthenticatedOrReadOnly,)
+
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
 
 
 class SnippetDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Snippet.objects.all()
     serializer_class = SnippetSerializer
+    permission_classes = (permissions.IsAuthenticatedOrReadOnly, IsOwnerOrReadOnly)
 
 
 """
--- web/django/tutorial/tutorial/urls.py
+++ web/django/tutorial/tutorial/urls.py
@@ -1,18 +1,21 @@
-from django.contrib import admin
-from django.urls import path
 from django.conf.urls import url, include
-from rest_framework import routers
-from quickstart import views
 
-router = routers.DefaultRouter()
-router.register(r'users', views.UserViewSet)
-router.register(r'groups', views.GroupViewSet)
+# from quickstart import views
+
+
+# router = routers.DefaultRouter()
+# router.register(r'users', views.UserViewSet)
+# router.register(r'groups', views.GroupViewSet)
 
 # urlpatterns = [
 #     path('admin/', admin.site.urls),
 # ]
+from snippets import views
+
 urlpatterns = [
     # url(r'^', include(router.urls)),
     url(r'^', include('snippets.urls')),
-    url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework'))
+    url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
+    url(r'^user/$', views.UserList.as_view()),
+    url(r'^users/(?P<pk>[0-9]+)/%', views.UserDetail.as_view())
 ]
